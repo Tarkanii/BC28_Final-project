7607e9a57c3c9422cf3f7899cd4ec552ca094a0a
Artur Tarkanii <86352980+Tarkanii@users.noreply.github.com>
2021-12-22 22:03:37 +0200

    .

--- src/navigation/Routes.jsx
+++ src/navigation/Routes.jsx
@@ -1,2 +1,14 @@
 import { Suspense, lazy } from "react";
 import { Switch } from "react-router-dom";
+import { PrivateRoute, PublicRoute } from "../shared/components/Routes";
+import AuthPage from "../pages/AuthPage";
+
+const Routes = () => {
+  <Suspense fallback={<div>Loading...</div>}>
+    <Switch>
+      <PublicRoute exact path="/" redirectTo="/login"></PublicRoute>
+      <PublicRoute exact path="/signup" redirectTo="/login"></PublicRoute>
+    </Switch>
+  </Suspense>;
+};
+export default Routes;
---
+++ src/shared/components/Routes/PrivatRoutes/PrivatRoute.jsx
@@ -0,0 +1,13 @@
+import { Route, Redirect } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
+
+const PrivateRoute = ({ children, redirectTo = "/", ...routeProps }) => {
+  const isLoggedIn = useAuth();
+  return (
+    <Route {...routeProps}>
+      {isLoggedIn ? children : <Redirect to={redirectTo} />}
+    </Route>
+  );
+};
+
+export default PrivateRoute;
