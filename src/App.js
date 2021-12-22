b24eeba7c7d998eeeb833f779717b379547babae
Artur Tarkanii <86352980+Tarkanii@users.noreply.github.com>
2021-12-22 18:23:47 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.scss";
-
+import Navbar from "./client/Navbar";
+import AuthPage from "./pages/AuthPage/AuthPage";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <AuthPage />
+    </div>
+  );
 }
 
 export default App;
