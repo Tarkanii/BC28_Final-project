a615791aefa418f7ce0ac6bb27ba660f23d04562
KliffUndersn <kliffundersn90@gmail.com>
2021-12-22 17:17:41 +0200

    initial murkup

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Navbar from "./client/Navbar";
 // import Inputs from "./shared/components/Input";
 // import Buttons from "./shared/components/Buttons";
 import Sidebar from "./shared/components/Sidebar";
+import AuthPage from "./pages/AuthPage/AuthPage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <AuthPage />
       {/* <Buttons /> */}
       <Sidebar />
       {/* <Inputs /> */}
