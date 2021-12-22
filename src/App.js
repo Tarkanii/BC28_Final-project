fb7fe3e18d8e82e401e59d22dbbef028ca2e832b
Artur Tarkanii <86352980+Tarkanii@users.noreply.github.com>
2021-12-22 23:05:12 +0200

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import "./App.scss";
 import Routes from "./navigation/Routes";
+import RegisterPage from "./pages/RegisterPage";
+import ProjectListPage from "./pages/ProjectListPage/ProjectListPage";
 import Navbar from "./client/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <SprintListPage /> */}
-      <ProjectListPage />
-      <RegisterPage />
-      {/* <Sidebar />
-      <Buttons />
-      <Inputs />
-      <Datepicker /> */}
+      <Routes />
     </div>
   );
 }
--- src/navigation/Routes.jsx
+++ src/navigation/Routes.jsx
@@ -1,10 +1,14 @@
 import { Suspense, lazy } from "react";
 import { Switch } from "react-router-dom";
-// import PrivateRoute from "../shared/components/Routes/PrivateRoutes";
+import PrivateRoute from "../shared/components/Routes/PrivateRoutes";
 import PublicRoute from "../shared/components/Routes/PublicRoutes";
 
 const LoginPage = lazy(() => import("../pages/LoginPage"));
 const RegisterPage = lazy(() => import("../pages/RegisterPage"));
+const ProjectListPage = lazy(() => import("../pages/ProjectListPage"));
+const ProjectPage = lazy(() => import("../pages/ProjectPage"));
+const SprintsListPage = lazy(() => import("../pages/SprintListPage"));
+const SprintPage = lazy(() => import("../pages/SprintPage"));
 
 const Routes = () => {
   return (
@@ -16,6 +20,22 @@ const Routes = () => {
         <PublicRoute exact path="/signup" redirectTo="/login">
           <RegisterPage />
         </PublicRoute>
+        <PublicRoute exact path="/login">
+          <LoginPage />
+        </PublicRoute>
+        <PrivateRoute exact path="/projects" redirectTo="/login">
+          <ProjectListPage />
+        </PrivateRoute>
+        <PrivateRoute exact path="/projects/:id/sprints" redirectTo="/login">
+          <ProjectPage />
+        </PrivateRoute>
+        <PrivateRoute
+          exact
+          path="/projects/:id/sprints/:id"
+          redirectTo="/login"
+        >
+          <SprintPage />
+        </PrivateRoute>
       </Switch>
     </Suspense>
   );
