0b55e8537e73a50298e454cbb81566d54e1726e3
Yevhen Danilov <danilov.evgen@gmail.com>
2021-12-23 14:02:11 +0200

    sidebar styles upd

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import "./App.scss";
-import Routes from "./navigation/Routes";
+// import Routes from "./navigation/Routes";
 import RegisterPage from "./pages/RegisterPage";
 import ProjectListPage from "./pages/ProjectListPage/ProjectListPage";
 import Navbar from "./client/Navbar";
-<<<<<<< HEAD
 // import Inputs from "./shared/components/Input";
 // import Buttons from "./shared/components/Buttons";
-// import Sidebar from "./shared/components/Sidebar";
+import Sidebar from "./shared/components/Sidebar";
 // import Datepicker from "./shared/components/Datepicker";
-=======
->>>>>>> 40d9357b43b3bab9fd712bf1f06818c2bbaff5a5
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-<<<<<<< HEAD
-      {/* <Sidebar />
-      <Buttons />
+      {/* <Routes /> */}
+      <Sidebar />
+      {/*<Buttons />
       <Inputs />
       <Datepicker /> */}
-=======
-      <Routes />
->>>>>>> 40d9357b43b3bab9fd712bf1f06818c2bbaff5a5
     </div>
   );
 }
