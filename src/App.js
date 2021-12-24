2ef831a647e60e2a837da57b33cf08179ccce02f
Artur Tarkanii <86352980+Tarkanii@users.noreply.github.com>
2021-12-24 16:24:09 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import "./App.scss";
 import Routes from "./navigation/Routes";
-import RegisterPage from "./pages/RegisterPage";
-import LoginPage from "./pages/LoginPage";
-import ProjectListPage from "./pages/ProjectListPage/ProjectListPage";
 import Navbar from "./client/Navbar";
-// import Inputs from "./shared/components/Input";
-// import Buttons from "./shared/components/Buttons";
-import Sidebar from "./shared/components/Sidebar";
-import ListElement from "./shared/components/ListElement";
-// import Datepicker from "./shared/components/Datepicker";
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes />
-      {/* <Sidebar /> */}
-      {/* <ListElement /> */}
-      {/*<Buttons />
-      <Inputs />
-      <Datepicker /> */}
     </div>
   );
 }
