ebf917bb97e0429a0aee7c9299e821a879d1ef0e
Artur Tarkanii <86352980+Tarkanii@users.noreply.github.com>
2021-12-25 19:27:33 +0200

    ff

--- src/pages/ProjectPage/SprintElement.jsx
+++ src/pages/ProjectPage/SprintElement.jsx
@@ -1,26 +1,34 @@
-import { elements } from "./elements";
 import styles from "./SprintListPage.module.scss";
 import DeleteButton from "../../shared/components/Buttons/deleteButton/DeleteButton";
+import { getAllSprints } from "../../redux/sprints/sprints-operations";
 const SprintElement = () => {
-  const addSprint = elements.map((item) => {
-    const keys = Object.values(item);
-    return keys.map((key) => {
-      return (
-        <li id={key.id} className={styles.SprintDesc}>
-          <p className={styles.Text}>{key.text}</p>
-          <p className={styles.Text}>{key.date}</p>
-        </li>
-      );
-    });
-  });
-  return (
-    <>
-      <ul className={styles.List}>{addSprint}</ul>
-      <DeleteButton className={styles.DeleteBtn} variant="secondary" />
-      {/* <button type="button" className={styles.DeleteBtn}>
-        <img className={styles.Img} src="#" width="20" alt="btn" />
-      </button> */}
-    </>
-  );
+  
+  // const sprints = useSelector(getAllSprints());
+  // const sprintsMarkup = sprints.map(sprint => {
+
+  // })
+  // const addSprint = elements.map(({id, startDate,}) => {
+  //   const keys = Object.values(item);
+  //   return keys.map((key) => {
+  //     return (
+  //       <li id={key.id} className={styles.SprintDesc}>
+  //         <p className={styles.Text}>{key.text}</p>
+  //         <p className={styles.Text}>{key.date}</p>
+  //       </li>
+  //     );
+  //   });
+  // });
+
+
+  // return ( 
+  //   <>
+  //     <ul className={styles.List}>{addSprint}</ul>
+  //     <DeleteButton className={styles.DeleteBtn} variant="secondary" />
+  //     {/* <button type="button" className={styles.DeleteBtn}>
+  //       <img className={styles.Img} src="#" width="20" alt="btn" />
+  //     </button> */}
+  //   </>
+  // );
+  return;
 };
 export default SprintElement;
