97705b4ce11f91f8707b2f8da31ec539a0c6b975
Artur Tarkanii <86352980+Tarkanii@users.noreply.github.com>
2021-12-25 13:03:37 +0200

    project page fixed

--- src/pages/SprintListPage/SprintElement.jsx
+++ src/pages/SprintListPage/SprintElement.jsx
@@ -1,27 +1,26 @@
-// import { elements } from "./elements";
+import { elements } from "./elements";
 import styles from "./SprintListPage.module.scss";
 import DeleteButton from "../../shared/components/Buttons/deleteButton/DeleteButton";
-
-const SprintElement = ({ _id, duration, startDate, endDate }) => {
-  // const addSprint = elements.map((item) => {
-  //   const keys = Object.values(item);
-  //   return keys.map((key) => {
+const SprintElement = () => {
+  const addSprint = elements.map((item) => {
+    const keys = Object.values(item);
+    return keys.map((key) => {
+      return (
+        <li id={key.id} className={styles.SprintDesc}>
+          <p className={styles.Text}>{key.text}</p>
+          <p className={styles.Text}>{key.date}</p>
+        </li>
+      );
+    });
+  });
   return (
     <>
-      <ul className={styles.List}>
-        <li id={_id} className={styles.SprintDesc}>
-          <p className={styles.Text}>Дата начала</p>
-          <p className={styles.Text}>{startDate}</p>
-        </li>
-      </ul>
+      <ul className={styles.List}>{addSprint}</ul>
       <DeleteButton className={styles.DeleteBtn} variant="secondary" />
-    </>
-  );
-  <>
-    {/* <button type="button" className={styles.DeleteBtn}>
+      {/* <button type="button" className={styles.DeleteBtn}>
         <img className={styles.Img} src="#" width="20" alt="btn" />
       </button> */}
-  </>;
+    </>
+  );
 };
-
 export default SprintElement;
--- src/pages/SprintListPage/SprintListPage.jsx
+++ src/pages/SprintListPage/SprintListPage.jsx
@@ -6,88 +6,54 @@ import AddNewSprintForm from "./AddNewSprintForm";
 import Sidebar from "../../shared/components/Sidebar/Sidebar";
 import Modal from "../../shared/components/Modal/Modal";
 import { useState } from "react";
-
 const SprintListPage = () => {
   const [showModal, setShowModal] = useState(false);
-  const [state, setState] = useState([]);
-
   const toggleModal = () => {
     setShowModal(!showModal);
   };
-
-  const addNewSprint = (sprint) => {
-    setState([...state, sprint]);
-  };
-
   // const menu = <div className={styles.modalItems}>{AddNewSprintForm}</div>;
-
   const closeModal = () => {
     setShowModal(false);
   };
-  const elements = state.map((item) => (
-    <div className={styles.SprintContainer}>
-      <div className={styles.SprintCard}>
-        <h3 className={styles.SprintName}>{item.name}</h3>
-        <SprintElement {...item} />
-      </div>
-    </div>
-  ));
   return (
-    <div className="container">
-      <div className={styles.Wrapper}>
-        <Sidebar item="project" />
-        <div className={styles.Sprint}>
-          <div className={styles.Header}>
-            <h2 className={styles.Title}>Project 1</h2>
-            <EditButton className={styles.EditBtn} />
-            <div className={styles.AddSprint}>
-              <AddButton
-                variant="item"
-                className={styles.AddSprintBtn}
-                onClick={toggleModal}
-              />
-              {showModal && (
-                <Modal closeModal={closeModal}>
-                  <AddNewSprintForm />
-                </Modal>
-              )}
-              <span className={styles.AddSprintText}>Создать спринт</span>
-            </div>
-          </div>
-          <div className={styles.AddMembers}>
-            <img
-              className={`${styles.Img} ${styles.MembersImg}`}
-              src="#"
-              width="20"
-              alt="btn"
-            />
-            {showModal && (
-              <Modal closeModal={closeModal}>
-                <AddNewSprintForm
-                  closeModal={closeModal}
-                  addNewSprint={addNewSprint}
+      <div className="container">
+        <div className={styles.Wrapper}>
+          <Sidebar item="project" />
+           <div className={styles.Sprint}>
+            <div className={styles.Header}>
+              <h2 className={styles.Title}>Project 1</h2>
+              <EditButton className={styles.EditBtn} />
+              <div className={styles.AddSprint}>
+                <AddButton
+                  variant="item"
+                  className={styles.AddSprintBtn}
+                  onClick={toggleModal}
                 />
-              </Modal>
-            )}
-            <span className={styles.AddSprintText}>Создать спринт</span>
-          </div>
-        </div>
-        <div className={styles.AddMembers}>
-          <img
-            className={`${styles.Img} ${styles.MembersImg}`}
-            src="#"
-            width="20"
-            alt="btn"
-          />
-          <button type="button" className={styles.AddBtn}>
-            Добавить людей
-          </button>
-        </div>
-        <div className={styles.SprintList}>
-          {/* <div className={styles.SprintContainer}>
-              <div className={styles.SprintCard}>
-                <h3 className={styles.SprintName}>Sprint Burndown Chart 1</h3>
-                <SprintElement />
+                {showModal && (
+                  <Modal closeModal={closeModal}>
+                    <AddNewSprintForm />
+                  </Modal>
+                )}
+                <span className={styles.AddSprintText}>Создать спринт</span>
+              </div>
+            </div>
+            <div className={styles.AddMembers}>
+              <img
+                className={`${styles.Img} ${styles.MembersImg}`}
+                src="#"
+                width="20"
+                alt="btn"
+              />
+              <button type="button" className={styles.AddBtn}>
+                Добавить людей
+              </button>
+            </div>
+            <div className={styles.SprintList}>
+              <div className={styles.SprintContainer}>
+                <div className={styles.SprintCard}>
+                  <h3 className={styles.SprintName}>Sprint Burndown Chart 1</h3>
+                  <SprintElement />
+                </div>
               </div>
               <div className={styles.SprintContainer}>
                 <div className={styles.SprintCard}>
@@ -107,11 +73,10 @@ const SprintListPage = () => {
                   <SprintElement />
                 </div>
               </div>
-            </div> */}
-        </div>
+            </div>
+          </div>
       </div>
     </div>
   );
 };
-
 export default SprintListPage;
