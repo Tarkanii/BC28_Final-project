d609d24be018b46a8be5589d9345f3e4179e084e
Anastasiia-Chikrizova <anastasiia.chikrizova@gmail.com>
2021-12-25 17:46:02 +0200

    fixed some bags

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,3 +1,5 @@
 export const fetchToken = ({ auth }) => auth.token;
 
 export const fetchEmail = ({ auth}) => auth.user.email;
+
+export const userCheck = ({auth}) => auth.isLogin;
--- src/shared/components/Sidebar/Sidebar.jsx
+++ src/shared/components/Sidebar/Sidebar.jsx
@@ -2,9 +2,17 @@ import SidebarItem from "./sidebarItem";
 import { AddButton } from "../Buttons";
 import styles from "./Sidebar.module.scss";
 import svg from "../../../assets/btnIcons/sprite.svg";
+import CreateProject from '../../../pages/ProjectListPage/CreateProject/CreateProject'
+import Modal from "../Modal/Modal";
+import { useState } from "react";
 
 function Sidebar({ item = "init item", children = `No ${item} added` }) {
   const title = `Show ${item}s`;
+
+  const [isOpen, setIsOpen] = useState(false);
+  const onCLick = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div className={styles.sidebar__wrapper}>
@@ -22,9 +30,14 @@ function Sidebar({ item = "init item", children = `No ${item} added` }) {
         {children}
       </ul>
       <div className={styles.sidebar__footer}>
-        <AddButton className={styles.sidebar__AddBtn} variant="item" />
+        <AddButton className={styles.sidebar__AddBtn} variant="item" onClick={onCLick}/>
         <p className={styles.sidebar__subscription}>Create a {item}</p>
       </div>
+      {(isOpen && item === "project") && 
+        <Modal closeModal={onCLick}>
+          <CreateProject onClick={onCLick} />
+        </Modal>
+      }
     </div>
   );
 }
