8d887f66ca3acef0d93df81bacb4436940e99b90
Artur Tarkanii <86352980+Tarkanii@users.noreply.github.com>
2021-12-26 19:22:51 +0200

    conflict fixed

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -8,7 +8,10 @@ import Modal from "../../shared/components/Modal/Modal";
 import { useState } from "react";
 import AddPeople from "./AddPeople";
 import { useDispatch } from "react-redux";
-import { getAllProjects, getProject } from "../../redux/projects/projects-operations";
+import {
+  getAllProjects,
+  getProject,
+} from "../../redux/projects/projects-operations";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -17,13 +20,17 @@ import DeleteButton from "../../shared/components/Buttons/deleteButton/DeleteBut
 
 const SprintListPage = () => {
   const [showModal, setShowModal] = useState(false);
-  const [showAddPeople, setShowAddPeople]=useState(false)
-  const {pathname} = useLocation()
+  const [showAddPeople, setShowAddPeople] = useState(false);
+  const { pathname } = useLocation();
 
-  const projectId = pathname.split('/projects/').join('/sprints').split('/sprints').join('')
-     //трудные времена трудеют трудных решений ^^^^ ///
+  const projectId = pathname
+    .split("/projects/")
+    .join("/sprints")
+    .split("/sprints")
+    .join("");
+  //трудные времена трудеют трудных решений ^^^^ ///
 
-    const sprintData = useSelector(getSprintData)
+  const sprintData = useSelector(getSprintData);
 
   const toggleModal = () => {
     setShowModal(!showModal);
@@ -32,57 +39,63 @@ const SprintListPage = () => {
     setShowAddPeople(!showAddPeople);
   };
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-      dispatch(getAllProjects());
-      dispatch(getProject(projectId))
-    }, [])
+  useEffect(() => {
+    dispatch(getAllProjects());
+    dispatch(getProject(projectId));
+  }, []);
 
-    
   // const menu = <div className={styles.modalItems}>{AddNewSprintForm}</div>;
   const closeModal = () => {
     setShowModal(false);
   };
 
-
   return (
-      <div className="container">
-        <div className={styles.Wrapper}>
-          {}
-          <Sidebar item="project"/>
-           <div className={styles.Sprint}>
-            <div className={styles.Header}>
-              <h2 className={styles.Title}>Project 1</h2>
-              <EditButton className={styles.EditBtn} />
-              <div className={styles.AddSprint}>
-                <AddButton
-                  variant="item"
-                  className={styles.AddSprintBtn}
-                  onClick={toggleModal}
-                />
-                {showModal && (
-                  <Modal closeModal={closeModal}>
-                    <AddNewSprintForm />
-                  </Modal>
-                )}
-                <span className={styles.AddSprintText}>Создать спринт</span>
-              </div>
+    <div className="container">
+      <div className={styles.Wrapper}>
+        <Sidebar item="project" />
+        <div className={styles.Sprint}>
+          <div className={styles.Header}>
+            <h2 className={styles.Title}>Project 1</h2>
+            <EditButton className={styles.EditBtn} />
+            <div className={styles.AddSprint}>
+              <AddButton
+                variant="item"
+                className={styles.AddSprintBtn}
+                onClick={toggleModal}
+              />
+              {showModal && (
+                <Modal closeModal={closeModal}>
+                  <AddNewSprintForm />
+                </Modal>
+              )}
+              <span className={styles.AddSprintText}>Создать спринт</span>
             </div>
-            <div className={styles.AddMembers}>
-            <AddButton variant="member" type="button" text="Добавить людей" className={styles.AddBtn} onClick={openModal}/>
-            {showAddPeople && (
-                  <Modal closeModal={openModal}>
-                    <AddPeople projectId={projectId} onClick={openModal} />
-                  </Modal>
-                )}
-            </div>
-            <ul className={styles.SprintList}>
-              {sprintData && sprintData.map((el) => {return (<SprintElement el={el} key={el._id} projectId={projectId}/>)
-              })}
-             
-            </ul>
           </div>
+          <div className={styles.AddMembers}>
+            <AddButton
+              variant="member"
+              type="button"
+              text="Добавить людей"
+              className={styles.AddBtn}
+              onClick={openModal}
+            />
+            {showAddPeople && (
+              <Modal closeModal={openModal}>
+                <AddPeople projectId={projectId} onClick={openModal} />
+              </Modal>
+            )}
+          </div>
+          <ul className={styles.SprintList}>
+            {sprintData &&
+              sprintData.map((el) => {
+                return (
+                  <SprintElement el={el} key={el._id} projectId={projectId} />
+                );
+              })}
+          </ul>
+        </div>
       </div>
     </div>
   );
